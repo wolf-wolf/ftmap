ce6d2d40febfd1038d6850631e18272714d77e7d
hunter <liming17@kuaishou.com>
2021-09-08 16:26:30 +0800

    feat: 增加基础帮助提示

--- src/cli.js
+++ src/cli.js
@@ -3,49 +3,51 @@ import getFileTree from './get-tree';
 import * as path from "path";
 import * as fs from "fs";
 import chalk from "chalk";
+import yargs from 'yargs';
 
 function parseArgumentsIntoOptions(rawArgs) {
-    console.log('rawArgs', rawArgs)
-    const args = arg({
-        '--tag': Boolean,       // 是否显示文件类型
-        '-T': '--tag',
+    const argv = yargs.usage("Usage: $0 [options]")
+        .alias('O', 'output')
+        .describe('O', '内容输出地址，默认为控制台打印')
 
-        '--level': Number,      // 显示的层级
-        '-L': '--level',
+        .alias('T', 'tag')
+        .describe('T', '是否展示文件类型')
 
-        '--pattern': String,    // exclude file
-        '-I': '--pattern',
+        .alias('L', 'level')
+        .describe('L', '文件目录结构的展示层级')
 
-        '--output': String,     // 输出
-        '-O': '--output'
-    }, {
-        argv: rawArgs.slice(2),
-    });
+        .alias('I', 'pattern')
+        .describe('I', '需要排除的文件目录正在表达式，eg. "/node_modules|.git/"')
+
+        .help('h')
+        .alias('h', 'help')
+        .showHelpOnFail(true, 'Specify --help for available options')
+        .argv;
 
     return {
-        tag: args['--tag'] || false,
-        pattern: args['--pattern'] || false,
-        level: args['--level'] || false,
-        output: args['--output'] || false,
+        tag: argv['tag'] || false,
+        pattern: argv['pattern'] || false,
+        level: argv['level'] || false,
+        output: argv['output'] || false,
     };
 }
 
 export async function cli(args) {
-    console.log(args)
     let options = parseArgumentsIntoOptions(args);
-    options.targetDirectory = options.targetDirectory || process.cwd();
 
-    console.log('%s', chalk.blue.bold('>> START 文件目录扫描....'));
-    const treeData = getFileTree(options.targetDirectory, options);
-    console.log('%s', chalk.green.bold('>> DONE 文件目录扫描...'));
-
-    if (options.output) {
-        console.log('%s', chalk.blue.bold(`>> START 文件写入....[${options.output}]`));
-        fs.writeFileSync(path.resolve(options.output), treeData);
-        console.log('%s', chalk.green.bold('>> DONE 文件写入....'));
-    } else {
-        console.log(treeData);
-    }
-
-    console.log('%s', chalk.green.bold('>> DONE 执行完成....'));
+    // options.targetDirectory = options.targetDirectory || process.cwd();
+    //
+    // console.log('%s', chalk.blue.bold('>> START 文件目录扫描....'));
+    // const treeData = getFileTree(options.targetDirectory, options);
+    // console.log('%s', chalk.green.bold('>> DONE 文件目录扫描...'));
+    //
+    // if (options.output) {
+    //     console.log('%s', chalk.blue.bold(`>> START 文件写入....[${options.output}]`));
+    //     fs.writeFileSync(path.resolve(options.output), treeData);
+    //     console.log('%s', chalk.green.bold('>> DONE 文件写入....'));
+    // } else {
+    //     console.log(treeData);
+    // }
+    //
+    // console.log('%s', chalk.green.bold('>> DONE 执行完成....'));
 }
